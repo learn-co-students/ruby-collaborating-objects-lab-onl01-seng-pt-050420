2be377a21f7b33e055c2ab84d3466bdc0428f5fe
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-02 20:01:44 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  
+  def songs 
+    Song.all.select{|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(song)
+    
+  end
+  
+  def print_songs
+    
+  end
+  
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,12 @@
+class MP3Importer
+  
+  def initialize(file_name)
+    @path = file_name
+  end
+  
+  def files
+    
+  end
+  
+  
+end
