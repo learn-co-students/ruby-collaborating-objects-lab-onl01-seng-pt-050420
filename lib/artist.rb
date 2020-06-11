26fb60d95ca046139b8f2255e993aa55d9926fbc
Sha-jay Perry <shajayjanae@gmail.com>
2020-06-11 04:55:29 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,49 +1,3 @@
-
-class MP3Importer
-
-  attr_reader :path
-
-    def initialize(path)
-      @path = path
-    end
-
-    def files
-      @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
-    end
-    def import
-      self.files.each{|file| Song.new_by_filename(file)}
-    end
-end
-
-
-class Song
-
-  attr_accessor :name, :artist
-  @@all = []
-  def initialize(name)
-    @name = name
-    @@all << self 
-  end
-  
-  def self.all
-    @@all 
-  end 
-
-  def self.new_by_filename(file)
-    song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    song = self.new(song_name)
-    song.artist_name = artist
-    song
-  end
-  
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
-  end
-
-end
-
 class Artist
 
   attr_accessor :name, :songs
@@ -83,7 +37,7 @@ class Artist
 
   def self.create(name)
     artist = self.new(name)
-    @@all << artist
+    # @@all << artist
     artist
   end
 
@@ -93,8 +47,11 @@ class Artist
     @@all << self
   end
 
-  def print_songs()
-    puts @songs.collect {|x| x.name}
-  end
+  def print_songs 
+		@songs.each do |song|
+			puts song.name
+		end 
+end
 
 end
+
