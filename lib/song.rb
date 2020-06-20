0c147cf46d4f65e5c64e47e6ea9100178bf9f267
Joshua Williams <jmw0426@hotmail.com>
2020-06-20 16:26:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,16 +13,22 @@ class Song
   end
   
   def self.new_by_filename(filename) 
-    song_name = filename.split(" - ")[1]
-    new_song = Song.new(song_name)
-    the_artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
-    new_song.artist = the_artist
-    new_song.artist.add_song(self)
-    new_song
+    # song_name = filename.split(" - ")[1]
+    # new_song = Song.new(song_name)
+    # the_artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    # new_song.artist = the_artist
+    # new_song.artist.add_song(self)
+    # new_song
+    
+    artist, title = filename.split(" - ")
+    self.new(title).tap { |song|
+      song.artist = Artist.find_or_create_by_name(artist)
+      song.artist.add_song(song)
+    }
   end
   
-  # def artist_name=(name)
-  #   (my_artist = self.all.find { |artist| artist.name == name }) ? my_artist : self.new(name)
-  # end
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by_name(name)
+  end
   
 end
