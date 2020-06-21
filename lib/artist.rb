f92d3ebad4e2f45d05ee19069b2acd00658b00e0
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 07:22:24 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist 
+
+attr_accessor :name 
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+   def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    artist_found = Artist.all.find {|a| a.name == name}
+    if artist_found == nil
+      new_artist = Artist.new(name)
+    else
+      artist_found
+    end
+  end 
+
+  def print_songs
+    @songs.each do |song| 
+      puts song.name
+    end 
+  end 
+
+end 
